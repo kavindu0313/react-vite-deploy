ec1072de6162e98cba4eca5b0bf697747e994952
Kavindu Senanayake <gihankavindu789@gmail.com>
2025-06-29 01:52:11 +0530

    FIX

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import { defineConfig } from "vite";
+iimport { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
   server: {
     host: "::",
     port: 8080,
@@ -20,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base:process.env.VITE_BASE_PATH||"react-vite-deploy",
 }));
